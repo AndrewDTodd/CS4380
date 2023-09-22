d49fe81ca4f1ea502020444349b94de23371f7a3
andrewdanieltodd@gmail.com <andrewdanieltodd@gmail.com>
2023-09-22 13:53:19 -0600

    Added shell scipt to automate steps of building

---
+++ Build.sh
@@ -0,0 +1,8 @@
+#list the available presets for user's information
+cmake --list-presets=all ./
+
+#set the preset configuration to the x64 release mode
+cmake --preset linux-x64-release
+
+#build directing cmake to the cache and configuration that should now be in the build/linux-x64-release directory
+cmake --build ./build/linux-x64-release
