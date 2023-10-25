8cd9532dfa4ea18f2500a0166d2e6071b7b0c43d
AndrewDTodd <44379289+AndrewDTodd@users.noreply.github.com>
2023-10-25 08:19:19 -0600

    Delete Build.sh

--- Build.sh
+++
@@ -1,8 +0,0 @@
-#list the available presets for user's information
-cmake --list-presets=all ./
-
-#set the preset configuration to the x64 release mode
-cmake --preset x64-Release
-
-#build directing cmake to the cache and configuration that should now be in the build/linux-x64-release directory
-cmake --build ./build/x64-Release
