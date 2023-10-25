014449859f3154aa250fe537687a3b05a78ad019
AndrewDTodd <44379289+AndrewDTodd@users.noreply.github.com>
2023-10-25 07:15:44 -0600

    Update to build script

--- Build.sh
+++ Build.sh
@@ -2,7 +2,7 @@
 cmake --list-presets=all ./
 
 #set the preset configuration to the x64 release mode
-cmake --preset linux-x64-release
+cmake --preset x64-release
 
 #build directing cmake to the cache and configuration that should now be in the build/linux-x64-release directory
-cmake --build ./build/linux-x64-release
+cmake --build ./build/x64-release
